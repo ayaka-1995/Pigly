36f8dddc347200349e1db94ef877e0c2997b578c
isii ayaka <ayaka950210@gmail.com>
2025-04-15 15:07:57 +0900

    css 変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -6,6 +6,10 @@
     <title>Pigly</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inika:wght@400;700&family=Lora:ital,wght@0,400..700;1,400..700&family=Noto+Serif+JP:wght@900&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="contact-form">
